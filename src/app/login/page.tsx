c4e39549455a7b17c05549f57938f0fd2bdb6d02
Caio Almeida <caioalmeidanaweb@gmail.com>
2025-06-01 23:16:00 -0400

    feat: add register form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import RegisterForm from "./_compoments/register-form";
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="login">
+          <TabsList>
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Criar Conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Faça o login para continuar</CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6"></CardContent>
+              <CardFooter>
+                <Button>Entrar</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="register">
+            <RegisterForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
