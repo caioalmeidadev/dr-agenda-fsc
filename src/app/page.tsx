69f52f679542a652ddafec365beb65fb2a4095a1
Caio Almeida <caioalmeidanaweb@gmail.com>
2025-06-05 13:10:51 -0400

    fix: routing home page loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  if (!session.user.plan) {
+    redirect("/new-subscription");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
 }
